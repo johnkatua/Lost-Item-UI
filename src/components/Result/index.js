d18040c5bb47086ab3d0ff579f0911548646048d
john katua <johnkatua99@gmail.com>
2022-06-06 13:27:07 +0300

    user can view contact info on different categories

--- src/components/Result/index.js
+++ src/components/Result/index.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 
+import Card from "../Cards/index"
+  
 const CardResult = () => {
   return (
-    <div>
-      Card Result
-    </div>
+    <Card />
   )
 }
 
